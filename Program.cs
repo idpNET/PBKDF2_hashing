22a00d3471965e405b2c8a2fff79d1dfbdb22ad0
Keyvan Hasani <k1.dsec993@gmail.com>
2023-05-04 18:13:28 +0330

    Changed RNGCryptoServiceProvider (obsolete) to RandomNumberGenerator Updated codes commenting Added console coloring Added Resource folder Updated README.md

--- Program.cs
+++ Program.cs
@@ -1,53 +1,81 @@
 ﻿/*  Password hashing using PBKDF2 (SHA256 hashing Algorithm)
- *  +Setting iteration time
- *  +Hashing process time tracking
+
+    .PBKDF2 Hashing + SHA256 Hashing Algorithm
+    .Strong Salting (an array of bytes with a cryptographically strong sequence of random nonzero values)
+    .Customizable Hashing and salting Keys' size + hashing iteration times.
+    .Hashing process time tracking.
+
  *  https://github.com/idpNET/PBKDF2_hashing
  */
 using System;
 using System.Diagnostics;
 using System.Drawing;
 using System.Runtime.InteropServices;
+using System.Text;
 
 namespace PBKDF2_hashing
 {
+
     internal class Program : HashGenerator
     {
+        #region Class Methods
         static void Main(string[] args)
         {
 
-            // Getting user password input in plain text for hashsing process
+            // Gets user password input in plain text for hashing process
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine("Please enter a password to hash:");
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.Red;
             var inputPassword = Console.ReadLine();
             Console.WriteLine("");
 
-            
+
             /*
-             * Getting iteration time for hashsing process 
-             * (How many times do you need HashAlgorith to run over your password hasing it? 
+             * Gets iteration time for hashing process 
+             * (How many times do you need HashAlgorith to run over your password hashing it? 
              * "Reportedly, safe values for production environments are in the hundreds of thousands"
             */
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine("Please enter iteration time:");
-            iterations = int.TryParse(Console.ReadLine(), out iterations) ? iterations : 10000;
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Iterations = int.TryParse(Console.ReadLine(), out Iterations) ? Iterations : 10000;
             Console.WriteLine("");
 
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.Green;
 
-            // Tracking hashing process elpased time [Lambda expression]
-            var time = HashGenerator.RunTimeMeasurement(() =>
+            // Tracks hashing process elapsed time [Lambda expression]
+            var Time = HashGenerator.RunTimeMeasurement(() =>
             {
-                // Calling HashIT to hash the input password
-                var hash = HashIt(inputPassword="default", out var salt);
+                // Calls HashIT to hash the input password
+                var Hash = ComputeBytesHash(inputPassword = "default", out var salt);
+                string HashToString = MergeBytesIntoString(Hash);
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine("Entered password is: " + inputPassword);
-                Console.WriteLine($"Password hash is : {hash}");
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"Password hash is : {HashToString}");
                 Console.WriteLine("");
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
                 Console.WriteLine($"Generated salt is : {Convert.ToHexString(salt)}");
 
             });
 
             // Results
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.Gray;
             Console.WriteLine("");
-            Console.WriteLine("** Hashing process took " + time + " with " + iterations + " times of iteration");
-
+            Console.WriteLine("[Hashing process took " + Time + " with " + Iterations + " times of iteration]");
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.White;
 
         }
+        #endregion
     }
 }
--- README.md
+++ README.md
@@ -1,6 +1,83 @@
-# Password hasing using PBKDF2 (SHA256 hashing Algorithm)
- C# console app
- *  First step is to run the project!
- *  Then you need to type your password and press Enter.
- *  Next, you need to specify iteration time (see codes' comments for more info) and press Enter.
- *  Results: Computed hash value based on the above settings and steps + salt value (for further verifications) + how much hashing process took
+<br/>
+<p align="center">
+  <a href="https://github.com/idpNET/PBKDF2_hashing">
+    <img src="Resources/logo.png" alt="Logo" width="128" height="128">
+  </a>
+
+  <h3 align="center">PBKDF2 Password Hasing with Salting (SHA256 Hashing Algorithm)</h3>
+
+  <p align="center">
+    This project provides a relatively decent and customizable hashing method using PBKDF2 and SHA256 hashing algorithm.
+    <br/>
+    <br/>
+    <a href="https://github.com/idpNET/PBKDF2_hashing"><strong>Explore the docs »</strong></a>
+    <br/>
+    <br/>
+    <a href="https://github.com/idpNET/PBKDF2_hashing/issues">Report Bug</a>
+    .
+    <a href="https://github.com/idpNET/PBKDF2_hashing/issues">Request Feature</a>
+  </p>
+</p>
+
+## Table Of Contents
+
+* [About the Project](#about-the-project)
+  * [Installation](#installation)
+* [Usage](#usage)
+* [Roadmap](#roadmap)
+* [Contributing](#contributing)
+* [License](#license)
+* [Authors](#authors)
+
+## About The Project
+
+This project provides a relatively decent and customizable hashing method using PBKDF2 (+ strong salting) and SHA256 hashing algorithm. Settable hashing and salting keys' size, along with the hashsing iteration times are some of the capabilities of the project.  
+
+Main Capabilities:
+
+* PBKDF2 Hashing + SHA256 Hashing Algorithm
+* Strong Salting (an array of bytes with a cryptographically strong sequence of random nonzero values)
+* Customizable Hashing and salting Keys' size + hashing iteration times.
+* Hashing process time tracking.
+
+### Installation
+
+1. Clone the repo in visual studio
+```sh
+https://github.com/idpNET/PBKDF2_hashing.git
+```
+2. Run the app and Get Started!
+
+## Usage
+
+Running the console app, you get prompted to enter the input password for hashing process (default password is 'default'). Next, you get prompted to enter the iteration value of hashing process (default is 1000). Hitting enter button, you get final results including the entered password, password hash value, salt value and the process time.
+
+P.S This project is well commented ! You can refer to the inline code comments for more information
+
+## Roadmap
+
+See the [open issues](https://github.com/idpNET/secure-facial-recognition-security-system-using-emguCV/issues) for a list of proposed features (and known issues).
+
+## Contributing
+
+Contributions are what make the open source community such an amazing place to be learn, inspire, and create. Any contributions you make are **greatly appreciated**.
+* If you have suggestions for adding or removing projects, feel free to [open an issue](https://github.com/idpNET/secure-facial-recognition-security-system-using-emguCV/issues/new) to discuss it, or directly create a pull request after you edit the *README.md* file with necessary changes.
+* Please make sure you check your spelling and grammar.
+* Create individual PR for each suggestion.
+* Please also read through the [Code Of Conduct](https://github.com/idpNET/secure-facial-recognition-security-system-using-emguCV/blob/main/CODE_OF_CONDUCT.md) before posting your first idea as well.
+
+### Creating A Pull Request
+
+1. Fork the Project
+2. Create your Feature Branch (`git checkout -b feature/AmazingFeature`)
+3. Commit your Changes (`git commit -m 'Add some AmazingFeature'`)
+4. Push to the Branch (`git push origin feature/AmazingFeature`)
+5. Open a Pull Request
+
+## License
+
+ licensed under the GNU General Public License v3.0
+
+## Authors
+
+* **Keyvan Hasani** - *Back-end developer and Ideapardaz.NET Administrator* - [Keyvan Hasani](https://github.com/idpNET) - *Developer*
